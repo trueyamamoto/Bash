aca0724dc945b15b25be46c743b15dcf8feb9311
trueyamamoto <62404220+trueyamamoto@users.noreply.github.com>
2020-07-04 22:11:28 +0900

    Update ReadLineAndReadInput.sh

--- ReadLineAndReadInput.sh
+++ ReadLineAndReadInput.sh
@@ -7,3 +7,6 @@ do
   read KEY
   echo $KEY $LINE
 done 3< $LIST
+
+# A file redirected to a while loop is received by the read command as standard input 0.
+# So that force the file to be redirected as a third file descriptor.
